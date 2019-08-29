6c09dbcc7e6e25be85175f532eecf5a9061e92b0
wahidikqbal <wahidikqbal@gmail.com>
2019-08-29 22:22:06 +0700

    14.Lambda

--- 14.Lambda.py
+++ 14.Lambda.py
@@ -1,1 +1,12 @@
-#membuat Lambda di Pyhton
+#fungsi biasa
+def jumlah(a,b):
+    c = a + b
+    return c
+
+hasil = jumlah(3,4)
+print(hasil)
+
+#fungsi dengan Lambda
+kali = lambda x,y: x*y
+hasilKali = kali(5,6)
+print(hasilKali)
