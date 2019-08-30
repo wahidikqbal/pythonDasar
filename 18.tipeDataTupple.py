29d946d41e0c169b36534b4eace64219ca6d9db8
wahidikqbal <wahidikqbal@gmail.com>
2019-08-30 18:13:05 +0700

    18. Tipe Data Tuple

---
+++ 18.tipeDataTupple.py
@@ -0,0 +1,11 @@
+#TIPE DATA LIST
+ganjil = [1,3,5,7,9,11] #-----> menggunakan kurung siku
+
+#TIPE DATA TUPLE
+genap = (2,4,6,8,10)    #-----> menggunakan kurung biasa
+
+print('DATA TUPLE')
+print('data Tuple tidak bisa berubah-ubah')
+print('data Tuple hanya memilik sedikit fungsi')
+print('data Tuple lebih kecil dari data List')
+print('data Tuple lebih cepat pengolahan data nya daripada data List')
