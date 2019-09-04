dd5cc6fd624d93886c08f66beaecf2ae0cacb9fa
wahidikqbal <wahidikqbal@gmail.com>
2019-09-04 19:02:25 +0700

    20.tipe data dictionary

---
+++ 20.tipeDataDictionary.py
@@ -0,0 +1,15 @@
+member1 = {
+    'nama'      : 'rudi',
+    'pekerjaan' : 'pengacara',
+    'hobby'     : 'masak',
+    'status'    : 'kawin',
+}
+
+member2 = {
+
+}
+
+print(member1['nama'])  #---> Print spesifik
+print(member1.keys())   #---> print berdasarkan kata kunci
+print(member1.values()) #---> print berdasarkan nilai
+print(member1.items())  #---> print berdasarkan 1 kelompok items
