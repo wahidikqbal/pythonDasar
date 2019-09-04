bae1adcbd1496e28f56f09382a26cbd420ac6c05
wahidikqbal <wahidikqbal@gmail.com>
2019-09-04 19:39:00 +0700

    catatan  module

--- 23.membuatModule.py
+++ 23.membuatModule.py
@@ -5,3 +5,8 @@ math.tambah(2,5)
 #import module tertentu
 from moduleKaliBagi import kali
 kali(5,7)
+
+
+#atau bisa juga dengan menggunakan
+#1.from moduleKaliBagi import * ---> import semua isi modul
+#2.form moduleKaliBagi import bagi as B ---> import fungsi Bagi dengan as
