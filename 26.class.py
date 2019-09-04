2c59f3b98b2738b9f6d71d37b3128c240745275e
wahidikqbal <wahidikqbal@gmail.com>
2019-09-04 21:28:22 +0700

    26. membuat class

---
+++ 26.class.py
@@ -0,0 +1,11 @@
+class siswa():
+    #pass ---> kalau class kosong pakai pass
+    fakultas = 'none'
+
+arifin = siswa()
+ucup   = siswa()
+
+arifin.fakultas = 'ekonomi'
+ucup.fakultas   = 'bahasa'
+
+print(arifin.fakultas)
