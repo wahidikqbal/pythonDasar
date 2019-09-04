37b078a53f6a64556fa6496067f18e1920e425d7
wahidikqbal <wahidikqbal@gmail.com>
2019-09-04 22:02:38 +0700

    24. method - 2

--- 27.method.py
+++ 27.method.py
@@ -1,13 +1,20 @@
 class siswa():
     #pass ---> kalau class kosong pakai pass
+    
     nama = 'masih kosong'
+
     def makan(self, argumen):
         print(self.nama, 'sedang makan', argumen)   #---> 1.self.nama memanggil nama
                                                     #---> 2.argumen memanggil argumen
+
+    def belajar(self, argumen):
+        print(self.nama, 'sedang belajar', argumen)
 
 arifin = siswa()
 ucup   = siswa()
 
 arifin.nama = 'arifin ilham'
+ucup.nama   = 'ucup markucup'
 
-arifin.makan('sayur bayem')  
+arifin.makan('sayur bayem') 
+ucup.belajar('python')
